26f953d2942a0b87f9a72882e70bc82e7fa5e081
Joaquin Druetta <joaquin.druetta@keylab.com.ar>
2022-03-11 09:15:34 -0300

    Exercise Bill Division completed

---
+++ Algorithms/02.Implementation/010.Bill Division.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    Anna = 0
+    for items in range (len(bill)):
+        if items != k:
+            Anna += bill[items]
+    if Anna/2 - b == 0:
+        print ('Bon Appetit')
+    else:
+        print (int(b - Anna/2))
+        
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
