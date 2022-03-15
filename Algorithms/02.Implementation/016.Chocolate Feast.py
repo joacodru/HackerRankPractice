372d0b055d5846bc0da894a6a1ca54123ad78419
Joaquin Druetta <joaquin.druetta@keylab.com.ar>
2022-03-15 17:04:26 -0300

    new excercise added

---
+++ Algorithms/02.Implementation/016.Chocolate Feast.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'chocolateFeast' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER c
+#  3. INTEGER m
+#
+
+def chocolateFeast(n, c, m):
+    bars = math.floor(n / c)
+    wraps = bars
+    while wraps >= m:
+        buyed = math.floor(wraps / m)
+        wraps = (wraps % m) + buyed
+        bars += buyed
+    return bars
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        n = int(first_multiple_input[0])
+
+        c = int(first_multiple_input[1])
+
+        m = int(first_multiple_input[2])
+
+        result = chocolateFeast(n, c, m)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
