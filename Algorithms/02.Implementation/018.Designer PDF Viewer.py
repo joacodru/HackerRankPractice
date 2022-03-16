2c5e11e0306bede6cebeb87ca9c6bdd2f9e35861
Joaquin Druetta <joaquin.druetta@keylab.com.ar>
2022-03-16 14:32:18 -0300

    New excercise completed

---
+++ Algorithms/02.Implementation/018.Designer PDF Viewer.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'designerPdfViewer' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY h
+#  2. STRING word
+#
+
+def designerPdfViewer(h, word):
+    asciValues = [h[ord(x) - 97] for x in word]
+    return max(asciValues) * len(word)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
